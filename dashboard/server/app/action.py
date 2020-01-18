80788e78402af9b46407704559358a78f9fa1e9c
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-01-18 23:08:54 +0900

    [server] 全APIのI/Fテスト完了、logsのみ取得したデータの中身がおかしい気がする

--- dashboard/server/app/action.py
+++ dashboard/server/app/action.py
@@ -9,6 +9,9 @@ from sqlalchemy.orm import sessionmaker
 from flask import Blueprint, request, jsonify, Response
 action = Blueprint("action", __name__, url_prefix="/action")
 
+from flask_cors import CORS
+CORS(action)
+
 @action.route("/open/<int:toilet_id>", methods=["POST"])
 def open(toilet_id: int) -> Response:
     """トイレのドアが開いたことを記録します。
@@ -34,8 +37,8 @@ def open(toilet_id: int) -> Response:
     if current_state == 0:
         # 停止モード
         return jsonify({
-            success: False,
-            message: "システムモードが停止状態です。すべての入退室ログは記録されません。"
+            "success": False,
+            "message": "システムモードが停止状態です。すべての入退室ログは記録されません。"
         })
 
     # 現在の在室状況を取得
@@ -43,14 +46,14 @@ def open(toilet_id: int) -> Response:
     if not is_closed:
         # 既に空室なので変化なし
         return jsonify({
-            success: False,
-            message: f"トイレ #{toilet_id} は既に空室です。重複防止のため退室ログは記録されません。"
+            "success": False,
+            "message": f"トイレ #{toilet_id} は既に空室です。重複防止のため退室ログは記録されません。"
         })
 
     # 現在の在室状況を更新
-    session
-        .query(Toilet)
-        .filter(Toilet.id == toilet_id)
+    session \
+        .query(Toilet) \
+        .filter(Toilet.id == toilet_id) \
         .query.update({
             Toilet.is_closed: False,
             Toilet.modified_time: dt.now()
@@ -65,8 +68,8 @@ def open(toilet_id: int) -> Response:
 
     session.commit()
     return jsonify({
-        success: True,
-        message: f"トイレ #{toilet_id} が空室になりました。"
+        "success": True,
+        "message": f"トイレ #{toilet_id} が空室になりました。"
     })
 
 @action.route("/close/<int:toilet_id>", methods=["POST"])
@@ -94,8 +97,8 @@ def close(toilet_id: int) -> Response:
     if current_state == 0:
         # 停止モード
         return jsonify({
-            success: False,
-            message: "システムモードが停止状態です。すべての入退室ログは記録されません。"
+            "success": False,
+            "message": "システムモードが停止状態です。すべての入退室ログは記録されません。"
         })
 
     # 現在の在室状況を取得
@@ -103,14 +106,14 @@ def close(toilet_id: int) -> Response:
     if is_closed:
         # 既に使用中なので変化なし
         return jsonify({
-            success: False,
-            message: f"トイレ #{toilet_id} は既に使用中です。重複防止のため入室ログは記録されません。"
+            "success": False,
+            "message": f"トイレ #{toilet_id} は既に使用中です。重複防止のため入室ログは記録されません。"
         })
 
     # 現在の在室状況を更新
-    session
-        .query(Toilet)
-        .filter(Toilet.id == toilet_id)
+    session \
+        .query(Toilet) \
+        .filter(Toilet.id == toilet_id) \
         .query.update({
             Toilet.is_closed: True,
             Toilet.modified_time: dt.now()
@@ -125,8 +128,8 @@ def close(toilet_id: int) -> Response:
 
     session.commit()
     return jsonify({
-        success: True,
-        message: f"トイレ #{toilet_id} が使用中になりました。"
+        "success": True,
+        "message": f"トイレ #{toilet_id} が使用中になりました。"
     })
 
 @action.route("/emergency", methods=["POST"])
@@ -146,26 +149,30 @@ def emergency() -> Response:
     session = sessionmaker(bind=engine)()
 
     # 現在の緊急停止モードを取得
-    current_state = session.query(AppState.state).filter(AppState.id == 1).first()
+    current_state = session \
+        .query(AppState.state) \
+        .filter(AppState.id == 1) \
+        .first() \
+        .state
 
     # 緊急停止モードを更新
-    if current_state == False:
-        next_state = False
+    if current_state == 1:
+        next_state = 0
         next_state_name = "停止"
     else:
-        next_state = True
+        next_state = 1
         next_state_name = "再開"
 
-    session
-        .query(AppState)
-        .filter(AppState.id == 1)
-        .query.update({
+    session \
+        .query(AppState) \
+        .filter(AppState.id == 1) \
+        .update({
             AppState.state: next_state,
             AppState.modified_time: dt.now()
         })
 
     session.commit()
     return jsonify({
-        valid: next_state,
-        action: next_state_name
+        "valid": next_state,
+        "action": next_state_name
     })
--- dashboard/server/app/main.py
+++ dashboard/server/app/main.py
@@ -2,11 +2,18 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+from flask_cors import CORS
+CORS(app)
+
+import sys
+sys.path.insert(0, ".")
+
 # 各種サブモジュールをロード
 from app.action import action
 from app.fetch import fetch
 app.register_blueprint(action)
 app.register_blueprint(fetch)
+
 
 
 @app.route("/health", methods=["GET"])
