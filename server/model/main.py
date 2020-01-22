620a1dbc6776def3e0686afa63f264fafbb541af
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-01-23 02:34:37 +0900

    [server] closed #5 入退室ログのグラフの出し方改善

--- server/model/main.py
+++ server/model/main.py
@@ -35,20 +35,20 @@ if __name__ == "__main__":
     ])
     # トイレマスター
     session.add_all([
-        Toilet(id=11, name="4F男性用トイレ", valid=False, is_closed=False, modified_time=now),
-        Toilet(id=21, name="6F男性用トイレ", valid=True, is_closed=False, modified_time=now),
-        Toilet(id=31, name="4F女性用トイレ", valid=False, is_closed=False, modified_time=now),
-        Toilet(id=32, name="4F女性用トイレ", valid=False, is_closed=False, modified_time=now),
-        Toilet(id=41, name="6F女性用トイレ", valid=False, is_closed=False, modified_time=now),
-        Toilet(id=42, name="6F女性用トイレ", valid=False, is_closed=False, modified_time=now)
+        Toilet(id=11, name="4F 男性用トイレ", valid=False, is_closed=False, modified_time=now),
+        Toilet(id=21, name="6F 男性用トイレ", valid=True, is_closed=False, modified_time=now),
+        Toilet(id=31, name="4F 女性用トイレ", valid=False, is_closed=False, modified_time=now),
+        Toilet(id=32, name="4F 女性用トイレ", valid=False, is_closed=False, modified_time=now),
+        Toilet(id=41, name="6F 女性用トイレ", valid=False, is_closed=False, modified_time=now),
+        Toilet(id=42, name="6F 女性用トイレ", valid=False, is_closed=False, modified_time=now)
     ])
-    # # [デバッグ用] トイレ入退室トランザクションテーブル
+    # # [テスト用] トイレ入退室トランザクションテーブル
     # import random
     # for toilet_id in [11, 21, 31, 32, 41, 42]:
-    #     count = random.randint(1, 10)
+    #     count = random.randint(1, 100)
     #     for i in range(count):
     #         timedelta_minutes = random.randint(1, 10000)
-    #         session.add(ToiletStatus(toilet_id=toilet_id, is_closed=False, created_time=now-timedelta(minutes=timedelta_minutes)))
+    #         session.add(ToiletStatus(toilet_id=toilet_id, is_closed=True, created_time=now-timedelta(minutes=timedelta_minutes)))
     # 初期状態で挿入しておきたいレコードの定義 ここまで <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     # 変更をコミット
